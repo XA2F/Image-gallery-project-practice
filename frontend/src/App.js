2fec468ca9a5a510a9ce612099d6def2e3ef9b1e
xa2f <Abidjanyapo@gmail.com>
2023-12-30 06:16:54 -0500

    Added search functionality and styling to the navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
-function App() {
+import Search from "./components/Search";
+
+const App = () => {
+	const [word, setWord] = useState("");
+	const handleSearchSubmit = (e) => {
+		e.preventDefault();
+		console.log(word);
+	};
+
+	console.log(word);
+
 	return (
 		<div>
 			<Header title="Images gallery" />
+			<Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
 		</div>
 	);
-}
+};
 
 export default App;
